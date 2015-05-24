d1cebedf003f79625fdccb6863ff8059e30c1f27
Rob Johansen <robert_johans22@hotmail.com>
2015-05-24 10:09:04 -0600

    More progress on the webpack config, including JSX.

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,15 @@
-document.write("It works!");
+require('../css/style.less');
+var React = require('react');
+
+var IntroText = React.createClass({
+  render: function() {
+    return (
+      <p>Hello, World!</p>
+    );
+  }
+});
+
+React.render(
+  <IntroText />,
+  document.getElementById('content')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   entry: './public/js/index.js',
   output: {
     path: '/Users/rob/code/rewordy.com/public/js',
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+      },
+      {
+        test: /.js$/,
+        loader: 'jsx-loader'
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("../css/bundle.min.css")
+  ]
 };
